c4d6ccd0e9271e5e61859e735f8c265a7d199e02
Rafael <68511324+GalassoX@users.noreply.github.com>
2022-08-23 19:42:26 -0500

    .gitignore fix

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,16 +2,14 @@ import { Router } from 'express';
 import { myUser, getPanelUser, getPanelUserByDiscord, updateUserServices } from '../controllers/user.controller';
 const router = Router();
 
-/*router.get('/pcu/user/:user', getPanelUser);
+router.get('/pcu/user/:user', getPanelUser);
 
-router.get('/samp/user/:name', myUser);*/
+router.get('/samp/user/:name', myUser);
 
 router.get('/user/discord/:id', getPanelUserByDiscord);
 
-//router.get('/user/update', updateUserServices);
+router.get('/user/update', updateUserServices);
 
-
-
-//router.get('/user/samp/:id', myUser);
+router.get('/user/samp/:id', myUser);
 
 export default router;
